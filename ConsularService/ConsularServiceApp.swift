a8b909dfcddca7254ce302da0868953abfec43a9
RAKHYUN <kyle2598@gmail.com>
2023-11-29 19:58:11 -0600

    Initial Commit

---
+++ ConsularService/ConsularServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsularServiceApp.swift
+//  ConsularService
+//
+//  Created by Rakhyun Kim on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ConsularServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
